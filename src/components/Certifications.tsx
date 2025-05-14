9cf4aa0864ce01f3c5686fe4a53461701787a023
Shudhanshu Ranjan <46088769+git4sudo@users.noreply.github.com>
2025-05-14 02:38:22 -0400

    Update Certifications.tsx

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -9,6 +9,12 @@ const certifications = [
     description: "Earned this industry-recognized certification to demonstrate expertise in TensorFlow, showcasing the ability to build, train, and deploy scalable machine learning models. Gained hands-on experience in developing and optimizing deep learning models using Python and TensorFlow."
   },
   {
+    title: "Reinforcement Learning",
+    image: "/assets/images/reinforcement-learning-cert.png",
+    link: "https://www.coursera.org/account/accomplishments/specialization/certificate/RIS448B7IY2Z", 
+    description: "Completed this four-course specialization by the University of Alberta, covering foundational and advanced reinforcement learning concepts. Gained practical experience with algorithms such as Temporal-Difference (TD) Learning, Monte Carlo Methods, Q-Learning, SARSA, and Policy Gradient techniques. Learned to model sequential decision-making problems using Markov Decision Processes (MDPs), implement value-based and policy-based methods, and evaluate agent performance. Developed end-to-end RL pipelines, reinforcing the ability to design intelligent agents in dynamic environments."
+  },
+  {
     title: "Deep Learning",
     image: "/assets/images/deep-learning-cert.png",
     link: "https://www.coursera.org/account/accomplishments/specialization/certificate/YK8FH7EW6N3L",
@@ -25,6 +31,12 @@ const certifications = [
     image: "/assets/images/gcp-cert.png",
     link: "https://www.coursera.org/account/accomplishments/specialization/certificate/63URWZDRE3XE",
     description: "Completed this five-course specialization, focused on designing and deploying scalable and reliable infrastructure solutions on Google Cloud Platform. Gained expertise in core infrastructure components such as networking, storage, and security, designing elastic infrastructure with autoscaling and load balancing, and implementing reliable solutions with monitoring, automation, and disaster recovery processes. Mastered hands-on practices in building applications using Google Cloud Compute Engine, preparing to handle complex cloud architecture challenges."
+  },
+  {
+    title: "DeepLearning.AI TensorFlow Developer",
+    image: "/assets/images/dlai-tfdeveloper-cert.png",
+    link: "https://www.coursera.org/account/accomplishments/specialization/certificate/ZL3LKVM35FHY",
+    description: "Completed this four-course specialization to gain practical experience in building scalable AI-powered applications using TensorFlow. The program covered key areas such as computer vision, natural language processing, and time series analysis. Gained hands-on experience with convolutional neural networks (CNNs), recurrent neural networks (RNNs), and long short-term memory networks (LSTMs). Learned to implement best practices for training and optimizing deep learning models, including techniques like data augmentation and dropout to prevent overfitting. Developed skills in processing real-world image and text data, and applied TensorFlow tools to build and deploy machine learning models effectively."
   }
 ];
 
