f87a56247317acaf2d216ee3e7fd491ca3df6071
Shudhanshu Ranjan <46088769+git4sudo@users.noreply.github.com>
2025-07-17 21:34:45 +0530

    Update Hero.tsx
    Updated location

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero: React.FC = () => {
           Shudhanshu Ranjan
         </h1>
         <div className="space-y-4">
-          <p className="text-xl md:text-2xl text-gray-200">New York City Metropolitan Area</p>
+          <p className="text-xl md:text-2xl text-gray-200">New York City Metropolitan Area, US | Bangalore, India</p>
           <p className="text-xl md:text-2xl text-gray-200">Data Scientist & Machine Learning Engineer with GCP Experience</p>
         </div>
         <p className="text-lg md:text-xl mt-8 mb-12 text-gray-300 max-w-3xl mx-auto">
